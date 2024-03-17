03286c0d3ed74f7ac48e25e786adbf42ddc0e421
Alex Ivanov <oleksii120@gmail.com>
2024-03-17 17:57:38 +0200

    Implemented board state change display on view (raw)

--- Renju/GlobalUsings.cs
+++ Renju/GlobalUsings.cs
@@ -1,4 +1,11 @@
 // Global using directives
 
 global using System.Windows;
-global using CommunityToolkit.Mvvm.ComponentModel;
+global using RenjuLib.Data;
+global using RenjuLib.Player;
+global using RenjuLib.Session;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Runtime.CompilerServices;
+global using System.Windows.Input;
+global using CommunityToolkit.Mvvm.Input;
--- Renju/ViewModels/RenjuViewModel.cs
+++ Renju/ViewModels/RenjuViewModel.cs
@@ -1,30 +1,82 @@
-using CommunityToolkit.Mvvm.Input;
-using RenjuLib.Data;
-using RenjuLib.Player;
-using RenjuLib.Session;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Renju.ViewModels;
 
-public class RenjuViewModel : ObservableObject
+public sealed class RenjuViewModel : ObservableObject
 {
     #region data
 
-    public ISession CurrentGameSession { get; set; }
+    private ISession CurrentGameSession { get; }
+
+    private ObservableCollection<Intersection>? _board;
+    public ObservableCollection<Intersection> Board
+    {
+        get => _board!;
+        private set => SetProperty(ref _board, value);
+    }
+    
+    private Intersection? _lastMove;
 
     #endregion
 
     #region commands
 
+    public ICommand BoardClickCmd { get; }
+    
     // add more commands here
 
     #endregion
 
+    #region constructors
+    
     public RenjuViewModel()
     {
+        BoardClickCmd = new RelayCommand<Intersection>(OnBoardClick);
+        
         // Grab the current game session from the database
-        CurrentGameSession = new SingleSession(
-            new HumanPlayer(CellStone.Black, "A"),
-            new HumanPlayer(CellStone.Black, "B")
-        );
+        var blackPlayer = new HumanPlayer(CellStone.Black, "A");
+        var whitePlayer = new HumanPlayer(CellStone.White, "B");
+        
+        blackPlayer.AwaitMove += MoveAwaiter(blackPlayer);
+        whitePlayer.AwaitMove += MoveAwaiter(whitePlayer);
+        
+        CurrentGameSession = new SingleSession(blackPlayer, whitePlayer);
+        CurrentGameSession.BoardChanged += OnBoardChanged;
+        OnBoardChanged();
+        
+        CurrentGameSession.Play();
     }
+    
+    #endregion
+
+    #region methods
+
+    private void OnBoardClick(Intersection? i)
+    {
+        if (i is null || i.Stone != CellStone.None)
+            return;
+        
+        _lastMove = i;
+    }
+
+    private Func<Task<Move>> MoveAwaiter(IPlayer player)
+        => async () =>
+        {
+            // Wait for the player to make a move
+            do
+            {
+                await Task.Delay(100);
+            } while (_lastMove is null);
+
+            // TODO: Add player info
+            var move = new Move(_lastMove.X, _lastMove.Y, player.Color);
+            _lastMove = null;
+            
+            return move;
+        };
+
+    private void OnBoardChanged() 
+        => Board = new(CurrentGameSession.CurrentRound.RenjuBoard.Intersections);
+    
+    #endregion
 }
