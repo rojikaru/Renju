7a9ea64f96b599bc8286c62fd790a63f39bf65ee
Alex Ivanov <oleksii120@gmail.com>
2024-04-08 17:01:46 +0300

    Added viewmodel and background

---
+++ Renju/ViewModels/RenjuViewModel.cs
@@ -0,0 +1,115 @@
+namespace Renju.ViewModels;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+public sealed class RenjuViewModel : ObservableObject
+{
+    #region data
+
+    private IMessageService MessageService { get; }
+
+    private ISession CurrentGameSession { get; }
+
+    private ObservableCollection<Intersection>? _board;
+
+    public ObservableCollection<Intersection> Board
+    {
+        get => _board!;
+        private set => SetProperty(ref _board, value);
+    }
+
+    private Intersection? _lastMove;
+
+    #endregion
+
+    #region commands
+
+    public ICommand BoardClickCmd { get; }
+
+    // add more commands here
+
+    #endregion
+
+    #region constructors
+
+    public RenjuViewModel(IMessageService messageService)
+    {
+        BoardClickCmd = new RelayCommand<Intersection>(OnBoardClick);
+        MessageService = messageService;
+
+        MessageService.ShowAsync(
+            "Game over!",
+            "It's a draw!",
+            "OK");
+
+        // Grab the current game session from the database
+        var blackPlayer = new HumanPlayer(CellStone.Black, "A");
+        var whitePlayer = new HumanPlayer(CellStone.White, "B");
+
+        blackPlayer.AwaitMove += CreateMoveAwaiter(blackPlayer);
+        whitePlayer.AwaitMove += CreateMoveAwaiter(whitePlayer);
+
+        CurrentGameSession = new SingleSession(blackPlayer, whitePlayer);
+        CurrentGameSession.BoardChanged += OnBoardChanged;
+        OnBoardChanged();
+
+        CurrentGameSession.GameEnded += async () =>
+        {
+            await MessageService.ShowAsync(
+                "Game over!",
+                CurrentGameSession.Result switch
+                {
+                    GameResult.BlackWon => "Black player wins!",
+                    GameResult.WhiteWon => "White player wins!",
+                    GameResult.Draw => "It's a draw!",
+                    _ => "Unknown result"
+                },
+                "OK"
+            );
+        };
+
+        CurrentGameSession.Play();
+    }
+
+    #endregion
+
+    #region methods
+
+    private void OnBoardClick(Intersection? i)
+    {
+        Console.WriteLine(i);
+
+        if (i is null)
+            return;
+
+        if (i.Stone != CellStone.Empty)
+        {
+            // TODO: Add some kind of error message
+            return;
+        }
+
+        _lastMove = i;
+    }
+
+    private Func<Task<Move>> CreateMoveAwaiter(IPlayer player)
+        => async () =>
+        {
+            // Wait for the player to make a move
+            do
+            {
+                await Task.Delay(400);
+            } while (_lastMove is null);
+
+            // TODO: Add player info
+            var move = new Move(_lastMove.X, _lastMove.Y, player.Color);
+            _lastMove = null;
+
+            return move;
+        };
+
+    private void OnBoardChanged()
+        => Board = new ObservableCollection<Intersection>(
+            CurrentGameSession.CurrentBoard.Intersections
+        );
+
+    #endregion
+}
