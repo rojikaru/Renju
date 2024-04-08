8161522b381f06024b1cf05f786e161e30a1129a
Alex Ivanov <oleksii120@gmail.com>
2024-04-08 17:00:43 +0300

    Added global usings to maui project

---
+++ Renju/GlobalUsings.cs
@@ -0,0 +1,13 @@
+// Global using directives
+
+global using System.Collections.ObjectModel;
+global using System.Windows.Input;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using Microsoft.Extensions.Logging;
+global using Renju.Models;
+global using Renju.ViewModels;
+global using Renju.Views;
+global using RenjuLib.Data;
+global using RenjuLib.Player;
+global using RenjuLib.Session;
--- Renju/Platforms/Android/MainApplication.cs
+++ Renju/Platforms/Android/MainApplication.cs
@@ -1,15 +1,11 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace Renju;
+namespace Renju.Platforms.Android;
 
 [Application]
-public class MainApplication : MauiApplication
+public class MainApplication(IntPtr handle, JniHandleOwnership ownership) 
+    : MauiApplication(handle, ownership)
 {
-    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-        : base(handle, ownership)
-    {
-    }
-
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
