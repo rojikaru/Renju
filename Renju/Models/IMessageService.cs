41576610232ea3e8725ba8477136b55cc346e44a
Alex Ivanov <oleksii120@gmail.com>
2024-04-20 20:45:59 +0300

    Improved message service

--- Renju/Models/IMessageService.cs
+++ Renju/Models/IMessageService.cs
@@ -3,4 +3,5 @@ namespace Renju.Models;
 public interface IMessageService
 {
     Task ShowAsync(string title, string message, string cancel);
+    Task<bool> ShowAsync(string title, string message, string accept, string cancel);
 }
--- Renju/Models/MessageService.cs
+++ Renju/Models/MessageService.cs
@@ -4,9 +4,19 @@ public class MessageService : IMessageService
 {
     public async Task ShowAsync(string title, string message, string cancel)
     {
-        await Application.Current?.MainPage?.Dispatcher.DispatchAsync(() =>
+        await Application.Current?.MainPage?.Dispatcher.DispatchAsync(async () =>
         {
-            Application.Current.MainPage.DisplayAlert(title, message, cancel);
+            await Application.Current.MainPage.DisplayAlert(title, message, cancel);
         })!;
+    }
+
+    public async Task<bool> ShowAsync(string title, string message, string accept, string cancel)
+    {
+        bool result = false;
+        await Application.Current?.MainPage?.Dispatcher.DispatchAsync(async () =>
+        {
+            result = await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+        })!;
+        return result;
     }
 }
